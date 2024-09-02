24031e2cd98f121fe3536c836a5e2e6208d02c34
Joshua S. Muse <sullivan.muse@gmail.com>
2024-09-02 13:09:22 -0500

    Format using black

--- comb.py
+++ comb.py
@@ -2,54 +2,56 @@ from dataclasses import dataclass, field
 from typing import Any
 import operator as op
 
+
 @dataclass
 class Input:
     s: str = field(default_factory=input)
     i: int = 0
 
     def __bool__(self):
-        '''
+        """
         >>> bool(Input('xy').advance(1))
         True
         >>> bool(Input('xy').advance(2))
         False
-        '''
+        """
         return self.i < len(self.s)
 
     def curr(self, n=1):
-        '''
+        """
         >>> Input('asdf').curr()
         'a'
         >>> Input('asdf').curr(2)
         'as'
-        '''
+        """
         return self.s[self.i : self.i + n]
-    
+
     def advance(self, n=1):
-        '''
+        """
         >>> Input('asdf').advance()
         Input(s='asdf', i=1)
         >>> Input('asdf').advance(2)
         Input(s='asdf', i=2)
-        '''
+        """
         return Input(self.s, self.i + n)
-    
+
     def span(self, other):
         return Span(self.s, self.i, other.i)
 
+
 @dataclass
 class Span:
-    s: str = ''
+    s: str = ""
     i: int = 0
     j: int = 0
-    
+
     @classmethod
     def all(cls, s):
         return cls(s, 0, len(s))
 
     def content(self):
         return self.s[self.i : self.j]
-    
+
     def span(self, other):
         if isinstance(other, Span):
             return Span(self.s, min(self.i, other.i), max(self.j, other.j))
@@ -57,6 +59,7 @@ class Span:
             return Span(self.s, min(self.i, other.i), max(self.j, other.i))
         else:
             raise TypeError
+
 
 @dataclass
 class Parser:
@@ -68,43 +71,46 @@ class Parser:
         elif isinstance(s, str):
             s = Input(s)
         return self.f(s)
-    
+
     def __add__(self, other):
-        '''
+        """
         >>> p = tag('x') + 'y'
         >>> p('z')
         >>> p('x')
         ('x', Input(s='x', i=1))
-        >>> p('y') 
+        >>> p('y')
         ('y', Input(s='y', i=1))
-        '''
+        """
         other = parser(other)
+
         @Parser
         def parse(s):
             return self(s) or other(s)
+
         return parse
-    
+
     def __radd__(self, other):
-        '''
+        """
         >>> p = 'x' + tag('y')
         >>> p('z')
         >>> p('x')
         ('x', Input(s='x', i=1))
-        >>> p('y') 
+        >>> p('y')
         ('y', Input(s='y', i=1))
-        '''
+        """
         other = parser(other)
         return other + self
-    
+
     def __mul__(self, other):
-        '''
+        """
         >>> p = tag('x') * 'y'
         >>> p('x')
-        >>> p('y') 
+        >>> p('y')
         >>> p('xy')
         (('x', 'y'), Input(s='xy', i=2))
-        '''
+        """
         other = parser(other)
+
         @Parser
         def parse(s):
             if r := self(s):
@@ -112,26 +118,27 @@ class Parser:
                 if r := other(s):
                     y, s = r
                     return (x, y), s
+
         return parse
-    
+
     def __rmul__(self, other):
-        '''
-        >>> p = 'x' * tag('y') 
+        """
+        >>> p = 'x' * tag('y')
         >>> p('x')
-        >>> p('y') 
+        >>> p('y')
         >>> p('xy')
         (('x', 'y'), Input(s='xy', i=2))
-        '''
+        """
         other = parser(other)
         return other * self
-    
+
     def __pow__(self, n):
-        '''
-        >>> p = tag('x') ** 5 
+        """
+        >>> p = tag('x') ** 5
         >>> p('xxx')
         >>> p('xxxxx')
         (('x', 'x', 'x', 'x', 'x'), Input(s='xxxxx', i=5))
-        '''
+        """
         if isinstance(n, int):
             if n >= 0:
                 return seq(*(self for _ in range(n)))
@@ -139,18 +146,19 @@ class Parser:
                 raise ValueError
         else:
             raise TypeError
-    
+
     def __getitem__(self, other):
-        '''
+        """
         >>> p = tag('x')[',']
-        >>> p('x') 
+        >>> p('x')
         ((['x'], []), Input(s='x', i=1))
         >>> p('x,x')
         ((['x', 'x'], [',']), Input(s='x,x', i=3))
         >>> p('x,x,')
         ((['x', 'x'], [',', ',']), Input(s='x,x,', i=4))
-        '''
+        """
         other = parser(other)
+
         @Parser
         def parse(s):
             xs = []
@@ -164,94 +172,98 @@ class Parser:
                 else:
                     break
             return (xs, ys), s
+
         return parse
-    
+
     def __lshift__(self, other):
-        '''
+        """
         >>> p = tag('x') << 'y'
         >>> p('xy')
         ('x', Input(s='xy', i=2))
-        '''
+        """
         return (self * other).left()
-    
+
     def __rlshift__(self, other):
-        '''
+        """
         >>> p = 'x' << tag('y')
         >>> p('xy')
         ('x', Input(s='xy', i=2))
-        '''
+        """
         other = parser(other)
         return other << self
-    
+
     def __rshift__(self, other):
-        '''
+        """
         >>> p = tag('x') >> 'y'
         >>> p('xy')
         ('y', Input(s='xy', i=2))
-        '''
+        """
         return (self * other).right()
-    
+
     def __rrshift__(self, other):
-        '''
+        """
         >>> p = 'x' >> tag('y')
         >>> p('xy')
         ('y', Input(s='xy', i=2))
-        '''
+        """
         other = parser(other)
         return other >> self
-    
+
     def __pos__(self):
-        '''
-        >>> p = +tag('x') 
+        """
+        >>> p = +tag('x')
         >>> p('x')
         ('x', Input(s='x', i=1))
-        >>> p(' x') 
+        >>> p(' x')
         ('x', Input(s=' x', i=2))
-        >>> p('  x') 
+        >>> p('  x')
         ('x', Input(s='  x', i=3))
-        >>> p('x ') 
+        >>> p('x ')
         ('x', Input(s='x ', i=1))
-        '''
+        """
         return ws >> self
-    
+
     def __neg__(self):
-        '''
+        """
         >>> p = -tag('x')
         >>> p(' x')
         >>> p('x')
         ('x', Input(s='x', i=1))
         >>> p('x ')
         ('x', Input(s='x ', i=2))
-        >>> p('x  ') 
+        >>> p('x  ')
         ('x', Input(s='x  ', i=3))
-        '''
+        """
         return self << ws
-    
+
     def optional(self):
-        '''
+        """
         >>> p = tag('x').optional()
         >>> p('x')
         ('x', Input(s='x', i=1))
         >>> p('')
         (None, Input(s='', i=0))
-        >>> p('y') 
+        >>> p('y')
         (None, Input(s='y', i=0))
-        '''
+        """
+
         @Parser
         def parse(s):
             return self(s) or (None, s)
+
         return parse
-    
+
     def many0(self):
-        '''
-        >>> p = tag('x').many0() 
+        """
+        >>> p = tag('x').many0()
         >>> p('')
         ([], Input(s='', i=0))
-        >>> p('xxxx') 
+        >>> p('xxxx')
         (['x', 'x', 'x', 'x'], Input(s='xxxx', i=4))
-        >>> p('x')    
+        >>> p('x')
         (['x'], Input(s='x', i=1))
-        '''
+        """
+
         @Parser
         def parse(s):
             xs = []
@@ -259,71 +271,78 @@ class Parser:
                 x, s = r
                 xs.append(x)
             return xs, s
+
         return parse
-    
+
     def many1(self):
-        '''
+        """
         >>> p = tag('x').many1()
         >>> p('')
         >>> p('x')
         (['x'], Input(s='x', i=1))
         >>> p('xxx')
         (['x', 'x', 'x'], Input(s='xxx', i=3))
-        '''
+        """
         return self.many0().pred(len)
-    
+
     def map(self, f):
-        '''
+        """
         >>> p = (tag('x') * 'y').map(lambda x: x[0])
         >>> p('x')
         >>> p('y')
         >>> p('xy')
         ('x', Input(s='xy', i=2))
-        '''
+        """
+
         @Parser
         def parse(s):
             if r := self(s):
                 x, s = r
                 return f(x), s
+
         return parse
-    
+
     def map_star(self, f):
-        '''
+        """
         >>> @dataclass
         ... class Two:
         ...     x: Any
         ...     y: Any
-        ... 
+        ...
         >>> p = (tag('x') * 'y').map_star(Two)
-        >>> p('x') 
+        >>> p('x')
         >>> p('y')
         >>> p('xy')
         (Two(x='x', y='y'), Input(s='xy', i=2))
-        '''
+        """
+
         @Parser
         def parse(s):
             if r := self(s):
                 x, s = r
                 return f(*x), s
+
         return parse
-    
+
     def pred(self, p):
-        '''
-        >>> p = (tag('x') + 'y').pred(lambda x: x != 'y') 
+        """
+        >>> p = (tag('x') + 'y').pred(lambda x: x != 'y')
         >>> p('x')
         ('x', Input(s='x', i=1))
         >>> p('y')
-        '''
+        """
+
         @Parser
         def parse(s):
             if r := self(s):
                 x, s1 = r
                 if p(x):
                     return x, s1
+
         return parse
-    
+
     def index(self, i):
-        '''
+        """
         >>> p1 = (tag('x') * 'y').index(0)
         >>> p1('x')
         >>> p1('y')
@@ -334,88 +353,96 @@ class Parser:
         >>> p2('y')
         >>> p2('xy')
         ('y', Input(s='xy', i=2))
-        '''
+        """
         return self.map(lambda x: x[i])
-    
+
     def left(self):
-        '''
-        >>> p = tag('x') * 'y'  
+        """
+        >>> p = tag('x') * 'y'
         >>> p1 = p.left()
         >>> p1('xy')
         ('x', Input(s='xy', i=2))
-        '''
+        """
         return self.index(0)
-    
+
     def right(self):
-        '''
+        """
         >>> p = tag('x') * 'y'
         >>> p2 = p.right()
         >>> p2('xy')
         ('y', Input(s='xy', i=2))
-        '''
+        """
         return self.index(1)
-    
+
     def spanned(self):
-        '''
+        """
         >>> p = (tag('x') * 'y').spanned()
         >>> p('xy')
         ((('x', 'y'), Span(s='xy', i=0, j=2)), Input(s='xy', i=2))
-        '''
+        """
+
         @Parser
         def parse(s):
             if r := self(s):
                 x, s1 = r
                 span = s.span(s1)
                 return (x, span), s1
+
         return parse
-    
+
     def span(self):
-        '''
+        """
         >>> p = (tag('x') * 'y').span()
         >>> p('xy')
         (Span(s='xy', i=0, j=2), Input(s='xy', i=2))
-        '''
+        """
         return self.spanned().right()
-    
+
     def stringed(self):
-        '''
+        """
         >>> p = (tag('x') * 'y').stringed()
         >>> p('xy')
         ((('x', 'y'), 'xy'), Input(s='xy', i=2))
-        '''
+        """
+
         @Parser
         def parse(s):
             if r := self(s):
                 x, s1 = r
                 string = s.span(s1).content()
                 return (x, string), s1
+
         return parse
-    
+
     def string(self):
-        '''
+        """
         >>> p = (tag('x') * 'y').string()
         >>> p('xy')
         ('xy', Input(s='xy', i=2))
-        '''
+        """
         return self.stringed().right()
-    
+
     def negate(self):
-        '''
-        >>> p = tag('x').negate() 
+        """
+        >>> p = tag('x').negate()
         >>> p('x')
         False
         >>> p('y')
         (None, Input(s='y', i=0))
-        '''
+        """
+
         @Parser
         def parse(s):
             return (not self(s)) and (None, s)
+
         return parse
+
 
 def make_parser(f):
     p = Parser()
     p.f = f(p)
     return p
+
 
 def parser(other):
     if isinstance(other, Parser):
@@ -427,24 +454,29 @@ def parser(other):
     else:
         raise TypeError
 
+
 def tag(m):
-    '''
+    """
     >>> tag('...')('')
     >>> tag('...')('...asdf')
     ('...', Input(s='...asdf', i=3))
-    '''
+    """
+
     @Parser
     def parse(s):
         if s.curr(len(m)) == m:
             return m, s.advance(len(m))
+
     return parse
 
+
 def seq(*ps):
-    '''
+    """
     >>> seq('x', 'y', 'z')('xyz')
     (('x', 'y', 'z'), Input(s='xyz', i=3))
-    '''
+    """
     ps = list(map(parser, ps))
+
     @Parser
     def parse(s):
         xs = []
@@ -455,37 +487,42 @@ def seq(*ps):
             else:
                 return
         return tuple(xs), s
+
     return parse
 
+
 def seqws(*ps):
-    '''
-    >>> p = seqws('x', 'y', 'z') 
+    """
+    >>> p = seqws('x', 'y', 'z')
     >>> p('xyz')
     (('x', 'y', 'z'), Input(s='xyz', i=3))
-    >>> p('x y z') 
+    >>> p('x y z')
     (('x', 'y', 'z'), Input(s='x y z', i=5))
-    >>> p('x y z ') 
+    >>> p('x y z ')
     (('x', 'y', 'z'), Input(s='x y z ', i=6))
-    >>> p('x  y  z  ') 
+    >>> p('x  y  z  ')
     (('x', 'y', 'z'), Input(s='x  y  z  ', i=9))
-    '''
+    """
     return seq(*(-parser(p) for p in ps))
 
+
 def seqspanned(*ps):
-    '''
+    """
     >>> p = seqspanned('x', 'y', 'z')
     >>> p('xy')
-    >>> p('xyz') 
+    >>> p('xyz')
     ((Span(s='xyz', i=0, j=3), 'x', 'y', 'z'), Input(s='xyz', i=3))
-    '''
+    """
     return seqws(*ps).spanned().map(lambda x: (x[1], *x[0]))
 
+
 def opleft(rators, rand):
-    '''
+    """
     rators: [(str, cls)]
-    '''
+    """
     rators = [(kw(rator), cls) for rator, cls in rators]
     rand <<= ws
+
     @Parser
     def parse(s):
         if r := rand(s):
@@ -508,13 +545,12 @@ def opleft(rators, rand):
                 else:
                     break
             return left, s1
+
     return parse
 
+
 def opright(rators, rand):
-    rators = [
-        (kw(rator), cls)
-        for rator, cls in rators
-    ]
+    rators = [(kw(rator), cls) for rator, cls in rators]
     rand <<= ws
     if len(rators) == 0:
         return rand
@@ -523,28 +559,31 @@ def opright(rators, rand):
         rator, cls = rators[0]
         p.f = seqspanned(rand, rator, p).map_star(cls)
 
-        for rator, cls in rators[1 :]:
+        for rator, cls in rators[1:]:
             p.f += seqspanned(rand, rator, p).map_star(cls)
         p.f += rand
         return p
 
+
 @Parser
 def one(s):
-    '''
+    """
     >>> one('x')
     ('x', Input(s='x', i=1))
     >>> one(Input('x').advance())
-    '''
+    """
     if s:
         return s.curr(), s.advance()
 
+
 def pred(p):
-    '''
+    """
     >>> pred(str.isdigit)('xyz')
     >>> pred(str.isdigit)('1234')
     ('1', Input(s='1234', i=1))
-    '''
+    """
     return one.pred(p)
+
 
 alpha = pred(str.isalpha)
 alnum = pred(str.isalnum)
@@ -554,6 +593,7 @@ upper = pred(str.isupper)
 space = pred(str.isspace)
 
 ws = space.many0()
+
 
 def kw(m):
     return -tag(m).span()
--- parse.py
+++ parse.py
@@ -2,15 +2,21 @@ from comb import *
 
 dchash = dataclass(eq=True, frozen=True)
 
+
 @dchash
 class Expr:
     span: Span
 
-@dchash
-class Int(Expr): pass
 
 @dchash
-class Id(Expr): pass
+class Int(Expr):
+    pass
+
+
+@dchash
+class Id(Expr):
+    pass
+
 
 @dchash
 class Paren(Expr):
@@ -18,54 +24,72 @@ class Paren(Expr):
     inner: Expr
     kw_rpar: Span
 
+
 @dchash
 class BinOp(Expr):
     left: Expr
     kw_op: Span
     right: Expr
 
+
 @dchash
-class Pow(BinOp): pass
+class Pow(BinOp):
+    pass
+
+
 @dchash
-class Mul(BinOp): pass
+class Mul(BinOp):
+    pass
+
+
 @dchash
-class Div(BinOp): pass
+class Div(BinOp):
+    pass
+
+
 @dchash
-class Add(BinOp): pass
+class Add(BinOp):
+    pass
+
+
 @dchash
-class Sub(BinOp): pass
+class Sub(BinOp):
+    pass
+
 
 run = digit.many1()
-integer = (run * ('_' * run).many0()).span().map(Int)
-name = (alpha * alnum.many0().optional() * ('_' * alnum.many1()).many0()).span()
+integer = (run * ("_" * run).many0()).span().map(Int)
+name = (alpha * alnum.many0().optional() * ("_" * alnum.many1()).many0()).span()
 ident = name.map(Id)
 atom = ident + integer
 
-pow = opright([('^', Pow)], atom)
-mul = opleft([('*', Mul), ('/', Div)], pow)
-add = opleft([('+', Add), ('-', Sub)], mul)
+pow = opright([("^", Pow)], atom)
+mul = opleft([("*", Mul), ("/", Div)], pow)
+add = opleft([("+", Add), ("-", Sub)], mul)
 
-paren = make_parser(lambda paren: seqspanned('(', paren, ')').map_star(Paren) + add)
+paren = make_parser(lambda paren: seqspanned("(", paren, ")").map_star(Paren) + add)
+
 
 def test_integer():
     s = "1234"
     assert integer(s) == (Int(Span.all(s)), Input(s, len(s))), "Successful parse"
-    
+
     s = "123_456"
     assert integer(s) == (Int(Span.all(s)), Input(s, len(s))), "Successful parse"
-    
+
     s = "_123"
     assert integer(s) is None, "Unsuccesful parse"
-    
+
     s = ""
     assert integer(s) is None, "Unsuccesful parse"
+
 
 def test_ident():
     s = "asdf"
     assert ident(s) == (Id(Span.all(s)), Input(s, len(s))), "Successful parse"
-    
+
     s = "Hello123"
     assert ident(s) == (Id(Span.all(s)), Input(s, len(s))), "Successful parse"
-    
+
     s = "1234asdf"
     assert ident(s) is None, "Unsuccessful parse"
