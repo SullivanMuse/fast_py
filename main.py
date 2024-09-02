819df2763378745173c73ac61e855325dcf19a62
Joshua S. Muse <sullivan.muse@gmail.com>
2024-09-02 13:07:11 -0500

    Reindent lines in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ dchash = dataclass(eq=True, frozen=True)
 
 @dchash
 class Expr:
-  span: Span
+    span: Span
 
 @dchash
 class Int(Expr): pass
@@ -14,15 +14,15 @@ class Id(Expr): pass
 
 @dchash
 class Paren(Expr):
-  kw_lpar: Span
-  inner: Expr
-  kw_rpar: Span
+    kw_lpar: Span
+    inner: Expr
+    kw_rpar: Span
 
 @dchash
 class BinOp(Expr):
-  left: Expr
-  kw_op: Span
-  right: Expr
+    left: Expr
+    kw_op: Span
+    right: Expr
 
 @dchash
 class Pow(BinOp): pass
@@ -48,24 +48,24 @@ add = opleft([('+', Add), ('-', Sub)], mul)
 paren = make_parser(lambda paren: seqspanned('(', paren, ')').map_star(Paren) + add)
 
 def test_integer():
-  s = "1234"
-  assert integer(s) == (Int(Span.all(s)), Input(s, len(s))), "Successful parse"
-  
-  s = "123_456"
-  assert integer(s) == (Int(Span.all(s)), Input(s, len(s))), "Successful parse"
-  
-  s = "_123"
-  assert integer(s) is None, "Unsuccesful parse"
-  
-  s = ""
-  assert integer(s) is None, "Unsuccesful parse"
+    s = "1234"
+    assert integer(s) == (Int(Span.all(s)), Input(s, len(s))), "Successful parse"
+    
+    s = "123_456"
+    assert integer(s) == (Int(Span.all(s)), Input(s, len(s))), "Successful parse"
+    
+    s = "_123"
+    assert integer(s) is None, "Unsuccesful parse"
+    
+    s = ""
+    assert integer(s) is None, "Unsuccesful parse"
 
 def test_ident():
-  s = "asdf"
-  assert ident(s) == (Id(Span.all(s)), Input(s, len(s))), "Successful parse"
-  
-  s = "Hello123"
-  assert ident(s) == (Id(Span.all(s)), Input(s, len(s))), "Successful parse"
-  
-  s = "1234asdf"
-  assert ident(s) is None, "Unsuccessful parse"
+    s = "asdf"
+    assert ident(s) == (Id(Span.all(s)), Input(s, len(s))), "Successful parse"
+    
+    s = "Hello123"
+    assert ident(s) == (Id(Span.all(s)), Input(s, len(s))), "Successful parse"
+    
+    s = "1234asdf"
+    assert ident(s) is None, "Unsuccessful parse"
