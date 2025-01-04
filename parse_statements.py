d60478eefc4fc224751127aad0dcb9b5a8d5bd1f
Joshua S. Muse <sullivan.muse@gmail.com>
2025-01-03 20:15:39 -0600

    Implement break, continue, return

--- parse_statements.py
+++ parse_statements.py
@@ -4,10 +4,39 @@ from tree import *
 from parse_expr import expr
 
 
-break_statement = integer = map("break", lambda span, _: Node(span, Statement.Break))
+break_statement = map("break", lambda span, _: Node(span, Statement.Break))
+continue_statement = map("continue", lambda span, _: Node(span, Statement.Continue))
+return_statement = map(seq("return", opt(ignore(ws), expr)), lambda span, rs: Node(span, Statement.Return, children=[rs[1]] if rs[1] is not None else [], tokens=[rs[0]]))
 
 autonomous_statement = alt("")
-semi_statement = alt(break_statement, expr)
+semi_statement = alt(break_statement, continue_statement, expr)
+
+
+def test_break_statement():
+    s = "break"
+    assert break_statement(s) == Success(Span(s, len(s), len(s)), Node(Span(s, 0, len(s)), Statement.Break))
+
+    s = ""
+    assert break_statement(s) == Error(Span(s, 0, None))
+
+
+def test_continue_statement():
+    s = "continue"
+    assert continue_statement(s) == Success(Span(s, len(s), len(s)), Node(Span(s, 0, len(s)), Statement.Continue))
+
+    s = ""
+    assert continue_statement(s) == Error(Span(s, 0, None))
+
+
+def test_return_statement():
+    s = "return"
+    assert return_statement(s) == Success(Span(s, len(s), len(s)), Node(Span(s, 0, len(s)), Statement.Return, tokens=[Span(s, 0, len(s))]))
+
+    s = "return x"
+    assert return_statement(s) == Success(Span(s, len(s), len(s)), Node(Span(s, 0, len(s)), Statement.Return, children=[Node(Span(s, 7, 8), Expr.Id)], tokens=[Span(s, 0, 6)]))
+
+    s = ""
+    assert return_statement(s) == Error(Span(s, 0, None))
 
 
 @Parser
