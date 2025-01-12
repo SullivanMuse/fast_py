dd154361c93df966ef1e681a8f0fd4f43586518c
Joshua S. Muse <sullivan.muse@gmail.com>
2025-01-12 16:10:08 -0600

    Refactor CST

--- parse_pattern.py
+++ parse_pattern.py
@@ -1,53 +1,51 @@
 from comb import *
+from parse_expr import name, dec_run, expr
 from tree import *
-from parse_expr import name, dec_run
 
 pattern = Parser()
 
-ignore_pattern = map(seq("_", name), lambda span, _: SyntaxNode(span, PatternTy.Ignore))
+ignore_pattern = map(seq("_", name), lambda span, _: IgnorePattern(span))
 
 
 def test_ignore_pattern():
     s = "_asdf"
-    node = SyntaxNode(Span(s, 0, len(s)), PatternTy.Ignore)
+    node = IgnorePattern(Span(s, 0, len(s)))
     assert ignore_pattern(s) == Success(Span(s, len(s), len(s)), node), "Success"
 
     s = ""
     assert ignore_pattern(s) == Error(Span(s, 0, None)), "Error"
 
 
-id_pattern = map(name, lambda span, _: SyntaxNode(span, PatternTy.Id))
+id_pattern = map(name, lambda span, _: IdPattern(span, span))
 
 
 def test_id_pattern():
     s = "asdf"
-    node = SyntaxNode(Span(s, 0, len(s)), PatternTy.Id)
+    node = IdPattern(Span(s, 0, len(s)), Span(s, 0, len(s)))
     assert id_pattern(s) == Success(Span(s, len(s), len(s)), node), "Success"
 
     s = ""
     assert id_pattern(s) == Error(Span(s, 0, None)), "Error"
 
 
-tag_pattern = map(
-    seq(ignore(":"), name), lambda span, r: SyntaxNode(span, PatternTy.Tag, tokens=[r])
-)
+tag_pattern = map(seq(ignore(":"), name), lambda span, _: TagPattern(span))
 
 
 def test_tag_pattern():
     s = ":asdf"
-    node = SyntaxNode(Span(s, 0, len(s)), PatternTy.Tag, tokens=[Span(s, 1, len(s))])
+    node = TagPattern(Span(s, 0, len(s)))
     assert tag_pattern(s) == Success(Span(s, len(s), len(s)), node), "Success"
 
     s = ""
     assert tag_pattern(s) == Error(Span(s, 0, None)), "Error"
 
 
-integer_pattern = map(dec_run, lambda span, _: SyntaxNode(span, PatternTy.Int))
+integer_pattern = map(dec_run, lambda span, _: IntPattern(span))
 
 
 def test_integer_pattern():
     s = "1234"
-    node = SyntaxNode(Span(s, 0, len(s)), PatternTy.Int)
+    node = IntPattern(Span(s, 0, len(s)))
     assert integer_pattern(s) == Success(Span(s, len(s), len(s)), node), "Success"
 
     s = ""
@@ -56,16 +54,10 @@ def test_integer_pattern():
 
 float_pattern = not_implemented("float_pattern")
 string_pattern = not_implemented("string_pattern")
-range_pattern = not_implemented("range_pattern")
 
 gather_pattern = starmap(
     seq("..", opt(pattern)),
-    lambda span, ellipsis, inner: SyntaxNode(
-        span,
-        PatternTy.Gather,
-        children=[] if inner is None else [inner],
-        tokens=[ellipsis],
-    ),
+    GatherPattern,
 )
 
 
@@ -73,11 +65,10 @@ def test_gather():
     s = "..x"
     assert gather_pattern(s) == Success(
         Span(s, len(s), len(s)),
-        SyntaxNode(
-            Span(s, 0, len(s)),
-            PatternTy.Gather,
-            children=[SyntaxNode(Span(s, 2, 3), PatternTy.Id)],
-            tokens=[Span(s, 0, 2)],
+        GatherPattern(
+            span=Span(s, 0, len(s)),
+            ellipsis=Span(s, 0, 2),
+            inner=IdPattern(Span(s, 2, 3), Span(s, 2, 3)),
         ),
     )
 
@@ -88,7 +79,7 @@ def array_pattern_items(s0):
     s = s0
 
     items = []
-    tokens = []
+    commas = []
 
     item = alt(pattern, gather_pattern)
     while r := item(s):
@@ -97,27 +88,31 @@ def array_pattern_items(s0):
         if not (r := sep(s)):
             break
         s = r.span
-        tokens.append(r.val)
+        commas.append(r.val)
 
-    gather_count = sum(1 for it in items if it.ty == PatternTy.Gather)
+    gather_count = sum(1 for item in items if isinstance(item, GatherPattern))
     if gather_count not in (0, 1):
         return Fail(s0.span(s), "Too many gather patterns")
 
     gather_index = 0
     for item in items:
-        if item.ty == PatternTy.Gather:
+        if isinstance(item, GatherPattern):
             break
         gather_index += 1
     else:
         gather_index = None
 
-    return Success(s, (items, tokens))
+    return Success(s, (items, commas))
 
 
 array_pattern = starmap(
     seq("[", ignore(ws), array_pattern_items, ignore(ws), "]"),
-    lambda span, lsq, items, rsq: SyntaxNode(
-        span, PatternTy.Array, children=items[0], tokens=[lsq, *items[1], rsq]
+    lambda span, lsq, items, rsq: ArrayPattern(
+        span,
+        items[0],
+        lsq,
+        items[1],
+        rsq,
     ),
 )
 
@@ -126,113 +121,111 @@ def test_array_pattern():
     s = "[1, 2, ..r, 4]"
     assert array_pattern(s) == Success(
         Span(s, len(s), len(s)),
-        SyntaxNode(
+        ArrayPattern(
             Span(s, 0, len(s)),
-            PatternTy.Array,
-            children=[
-                SyntaxNode(Span(s, 1, 2), PatternTy.Int),
-                SyntaxNode(Span(s, 4, 5), PatternTy.Int),
-                SyntaxNode(
-                    Span(s, 7, 10),
-                    PatternTy.Gather,
-                    children=[SyntaxNode(Span(s, 9, 10), PatternTy.Id)],
-                    tokens=[Span(s, 7, 9)],
+            items=[
+                IntPattern(Span(s, 1, 2)),
+                IntPattern(Span(s, 4, 5)),
+                GatherPattern(
+                    span=Span(s, 7, 10),
+                    ellipsis=Span(s, 7, 9),
+                    inner=IdPattern(Span(s, 9, 10), Span(s, 9, 10)),
                 ),
-                SyntaxNode(Span(s, 12, 13), PatternTy.Int),
+                IntPattern(Span(s, 12, 13)),
             ],
-            tokens=[
-                Span(s, 0, 1),
+            lsq=Span(s, 0, 1),
+            commas=[
                 Span(s, 2, 3),
                 Span(s, 5, 6),
                 Span(s, 10, 11),
-                Span(s, 13, 14),
             ],
+            rsq=Span(s, 13, 14),
         ),
     )
 
     s = "[]"
     assert array_pattern(s) == Success(
         Span(s, len(s), len(s)),
-        SyntaxNode(
-            Span(s, 0, len(s)),
-            PatternTy.Array,
-            children=[],
-            tokens=[Span(s, 0, 1), Span(s, 1, 2)],
+        ArrayPattern(
+            span=Span(s, 0, len(s)),
+            items=[],
+            lsq=Span(s, 0, 1),
+            commas=[],
+            rsq=Span(s, 1, 2),
         ),
     )
 
     s = "[..r]"
     assert array_pattern(s) == Success(
         Span(s, len(s), len(s)),
-        SyntaxNode(
-            Span(s, 0, len(s)),
-            PatternTy.Array,
-            children=[
-                SyntaxNode(
-                    Span(s, 1, len(s) - 1),
-                    PatternTy.Gather,
-                    children=[
-                        SyntaxNode(Span(s, len(s) - 2, len(s) - 1), PatternTy.Id)
-                    ],
-                    tokens=[Span(s, 1, 3)],
+        ArrayPattern(
+            span=Span(s, 0, len(s)),
+            items=[
+                GatherPattern(
+                    span=Span(s, 1, len(s) - 1),
+                    ellipsis=Span(s, 1, 3),
+                    inner=IdPattern(
+                        Span(s, len(s) - 2, len(s) - 1), Span(s, len(s) - 2, len(s) - 1)
+                    ),
                 )
             ],
-            tokens=[Span(s, 0, 1), Span(s, len(s) - 1, len(s))],
+            lsq=Span(s, 0, 1),
+            commas=[],
+            rsq=Span(s, len(s) - 1, len(s)),
         ),
     )
 
     s = "[a, ..r]"
     assert array_pattern(s) == Success(
         Span(s, len(s), len(s)),
-        SyntaxNode(
-            Span(s, 0, len(s)),
-            PatternTy.Array,
-            children=[
-                SyntaxNode(Span(s, 1, 2), PatternTy.Id),
-                SyntaxNode(
-                    Span(s, len(s) - 4, len(s) - 1),
-                    PatternTy.Gather,
-                    children=[
-                        SyntaxNode(Span(s, len(s) - 2, len(s) - 1), PatternTy.Id),
-                    ],
-                    tokens=[Span(s, len(s) - 4, len(s) - 2)],
+        ArrayPattern(
+            span=Span(s, 0, len(s)),
+            items=[
+                IdPattern(Span(s, 1, 2), Span(s, 1, 2)),
+                GatherPattern(
+                    span=Span(s, len(s) - 4, len(s) - 1),
+                    ellipsis=Span(s, len(s) - 4, len(s) - 2),
+                    inner=IdPattern(
+                        Span(s, len(s) - 2, len(s) - 1), Span(s, len(s) - 2, len(s) - 1)
+                    ),
                 ),
             ],
-            tokens=[Span(s, 0, 1), Span(s, 2, 3), Span(s, len(s) - 1, len(s))],
+            lsq=Span(s, 0, 1),
+            commas=[Span(s, 2, 3)],
+            rsq=Span(s, len(s) - 1, len(s)),
         ),
     )
 
     s = "[..r, a]"
     assert array_pattern(s) == Success(
         Span(s, len(s), len(s)),
-        SyntaxNode(
-            Span(s, 0, len(s)),
-            PatternTy.Array,
-            children=[
-                SyntaxNode(
-                    Span(s, 1, 4),
-                    PatternTy.Gather,
-                    children=[
-                        SyntaxNode(Span(s, 3, 4), PatternTy.Id),
-                    ],
-                    tokens=[Span(s, 1, 3)],
+        ArrayPattern(
+            span=Span(s, 0, len(s)),
+            items=[
+                GatherPattern(
+                    span=Span(s, 1, 4),
+                    ellipsis=Span(s, 1, 3),
+                    inner=IdPattern(Span(s, 3, 4), Span(s, 3, 4)),
                 ),
-                SyntaxNode(Span(s, 6, 7), PatternTy.Id),
+                IdPattern(Span(s, 6, 7), Span(s, 6, 7)),
             ],
-            tokens=[Span(s, 0, 1), Span(s, 4, 5), Span(s, len(s) - 1, len(s))],
+            lsq=Span(s, 0, 1),
+            commas=[Span(s, 4, 5)],
+            rsq=Span(s, len(s) - 1, len(s)),
         ),
     )
 
     s = "[a]"
     assert array_pattern(s) == Success(
         Span(s, len(s), len(s)),
-        SyntaxNode(
-            Span(s, 0, len(s)),
-            PatternTy.Array,
-            children=[
-                SyntaxNode(Span(s, 1, 2), PatternTy.Id),
+        ArrayPattern(
+            span=Span(s, 0, len(s)),
+            items=[
+                IdPattern(Span(s, 1, 2), Span(s, 1, 2)),
             ],
-            tokens=[Span(s, 0, 1), Span(s, len(s) - 1, len(s))],
+            lsq=Span(s, 0, 1),
+            commas=[],
+            rsq=Span(s, len(s) - 1, len(s)),
         ),
     )
 
@@ -245,6 +238,5 @@ pattern.f = alt(
     float_pattern,
     string_pattern,
     array_pattern,
-    range_pattern,
     gather_pattern,
 )
--- tree.py
+++ tree.py
@@ -1,73 +1,22 @@
-from comb import Span
 from dataclasses import dataclass, field
-from enum import auto, Enum
 from typing import Optional
 
-from format_node import FormatNode
-
-
-class ExprTy(Enum):
-    # No free variables
-    Int = auto()  # 123
-    Tag = auto()  # :a
-    Float = auto()  # 123.456
-    String = auto()  # d"Hello\\, {x}"
-
-    # Union of children
-    Array = auto()  # [e0, e1, ...]
-    Spread = auto()  # ...e0
-    Paren = auto()  # (e0)
-    Call = auto()  # e0(e1, e2, ...)
-    Index = auto()  # e0[e1, e2, ...]
-    Binary = auto()  # e0**e1
-    Unary = auto()  # !e0 e0.await e0.chain e0? e0.x
-    Comparison = auto()  # x is y isnot z
-
-    # Special
-    Id = auto()  # a
-    Fn = auto()  # fn(p0, p1, ...) e0
-    Block = auto()  # { s0, s1, ... }
-    Match = auto()  # match e0 { arm... }
-    Loop = auto()  # loop { ... }
-
-
-class PatternTy(Enum):
-    Ignore = auto()  # _name
-
-    Id = auto()  # a[@p]
-    Tag = auto()  # :a
-    Int = auto()  # 123
-    Float = auto()  # 123.456
-    String = auto()  # "Hello"
-
-    Array = auto()  # [p0, ...p1, p2]
-    Range = auto()  # [a]..[=][b]
-    Gather = auto()  # ..r
-
-    # TODO: {e0} dynamic literal pattern
-    # TODO: p0 <- e0 Pattern guards
-
-
-class StatementTy(Enum):
-    Expr = auto()
-    Let = auto()  # let p = e
-    Assign = auto()  # p = e
-    Loop = auto()  # loop { ... }
-    Match = auto()  # match subject { arm... }
-    Break = auto()  # break [x]
-    Continue = auto()  # continue
-    Return = auto()  # return [x]
+from comb import Span
+from mixins import FormatNode
 
 
 @dataclass
 class SyntaxNode(FormatNode):
     span: Span
-    ty: ExprTy | StatementTy | PatternTy
-    children: list["SyntaxNode"] = field(default_factory=list)
-    op: Optional[Span] = None
 
-    # Tokens that are not syntax nodes, like { } ; ,
-    tokens: list[Span] = field(default_factory=list)
+    @property
+    def children(self):
+        if hasattr("inner"):
+            return [self.inner]
+        elif hasattr("items"):
+            return self.items
+        else:
+            return ()
 
     def str(self):
         return f"{self.ty.name} {self.span}"
@@ -76,108 +25,501 @@ class SyntaxNode(FormatNode):
 @dataclass
 class Expr(SyntaxNode):
     def free(self, set_=None) -> set[str]:
-        if not isinstance(self.ty, ExprTy):
-            raise TypeError
-
         if set_ is None:
             set_ = set()
 
-        match self.ty:
-            # No free variables
-            case ExprTy.Int | ExprTy.Tag | ExprTy.Float | ExprTy.String:
-                pass
+        match self:
+            case IdExpr(_, name, inner):
+                set_.add(name.str())
+                if inner is not None:
+                    inner.free(set_)
 
-            # Union of children
-            case (
-                ExprTy.Array
-                | ExprTy.Spread
-                | ExprTy.Paren
-                | ExprTy.Call
-                | ExprTy.Index
-                | ExprTy.Binary
-                | ExprTy.Unary
-                | ExprTy.Comparison
-            ):
-                for child in self.children:
-                    child.free(set_)
-
-            # Special
-            case ExprTy.Id:
-                set_.add(self.span.str())
-
-            case ExprTy.Fn:
+            case FnExpr(_, _, inner):
                 patterns = []
                 body = None
                 for child in self.children:
-                    if isinstance(child.ty, PatternTy):
+                    if isinstance(child, Pattern):
                         patterns.append(child)
                     else:
                         body = child
                 body.free(set_)
                 for pat in patterns:
-                    pat.remove_free(set_)
+                    pat.remove_bound(set_)
 
-            case ExprTy.Block:
-                self.scope_free(set_)
+            case LoopExpr(_, statements):
+                free(statements, set_)
 
-            case ExprTy.Loop:
-                self.scope_free(set_)
+            case (
+                ArrayExpr(_)
+                | Spread(_)
+                | ParenExpr(_)
+                | CallExpr(_)
+                | IndexExpr(_)
+                | BinaryExpr(_)
+                | UnaryExpr(_)
+                | ComparisonExpr(_)
+            ):
+                for child in self.children:
+                    child.free(set_)
 
-            case ExprTy.Match:
-                self.children[0].free(set_)
-                self.scope_free(set_, start_index=1)
+            case IntExpr(_) | TagExpr(_) | FloatExpr(_) | StringExpr(_):
+                pass
+
+            case _:
+                raise NotImplementedError(f"`{type(self)}.free`")
 
         return set_
+
+
+@dataclass
+class IntExpr(Expr):
+    """Integer literal expression
+
+    Example:
+        123
+    """
+
+    pass
+
+
+@dataclass
+class TagExpr(Expr):
+    """Tag literal expression
+
+    Example:
+        :a
+    """
+
+    pass
+
+
+@dataclass
+class FloatExpr(Expr):
+    """Float literal expression
+
+    Example:
+        123.456
+    """
+
+    pass
+
+
+@dataclass
+class StringExpr(Expr):
+    """String literal expression
+
+    Example:
+        d"Hello\\, {x}"
+    """
+
+    fn: Optional[Span]
+    items: list[str | Expr]
+
+
+@dataclass
+class ArrayExpr(Expr):
+    """Array literal expression
+
+    Example:
+        [e0, e1, ...]
+    """
+
+    items: list[Expr]
+
+
+@dataclass
+class Spread(Expr):
+    """Spread operator pseudo-expression
+
+    May only appear within an array literal expression or a struct literal expression
+
+    Example:
+        ...e0
+    """
+
+    inner: Expr
+
+
+@dataclass
+class ParenExpr(Expr):
+    """Parenthesized expression
+
+    Example:
+        (e0)
+    """
+
+    pass
+
+
+@dataclass
+class CallExpr(Expr):
+    """Call expression
+
+    Example:
+        e0(e1, e2, ...)
+    """
+
+    pass
+
+
+@dataclass
+class IndexExpr(Expr):
+    """Index expression
+
+    Example:
+        e0[e1, e2, ...]
+    """
+
+    pass
+
+
+@dataclass
+class BinaryExpr(Expr):
+    """Binary operator expression
+
+    Example:
+        e0**e1
+    """
+
+    pass
+
+
+@dataclass
+class UnaryExpr(Expr):
+    """Unary operator expression
+
+    Example:
+        !e0 e0.await e0.chain e0? e0.x
+    """
+
+    pass
+
+
+@dataclass
+class ComparisonExpr(Expr):
+    """Comparison expression
+
+    Example:
+        x is y isnot z
+    """
+
+    pass
+
+
+@dataclass
+class IdExpr(Expr):
+    """Identifier
+
+    Example:
+        a
+    """
+
+    pass
+
+
+@dataclass
+class FnExpr(Expr):
+    """Function expression
+
+    Example:
+        fn(p0, p1, ...) e0
+    """
+
+    params: list["Pattern"]
+    inner: Expr
+
+
+@dataclass
+class BlockExpr(Expr):
+    """Block expression
+
+    Example:
+        { s0, s1, ... }
+    """
+
+    statements: list["Statement"]
+
+
+@dataclass
+class MatchExpr(Expr):
+    """Match expression
+
+    Example:
+        match e0 { arm... }
+    """
+
+    subject: Expr
+    arms: list[tuple["Pattern", Expr]]
+
+
+@dataclass
+class LoopExpr(Expr):
+    """Loop expression
+
+    Example:
+        loop { ... }
+    """
+
+    statements: list["Statement"]
+
+
+def free(statements, set_=None) -> set[str]:
+    if set_ is None:
+        set_ = set()
+    scope = set()
+    for statement in statements:
+        match statement:
+            case ExprStatement(_, inner):
+                inner.free(set_)
+
+            case LetStatement(_, pattern, inner):
+                pattern.binds(scope)
+                set_ += inner.free() - scope
+
+            case AssignStatement(_, pattern, inner):
+                set_ += inner.free() - scope
+
+            case LoopStatement(_, statements):
+                free(statements, set_)
+
+            case MatchStatement(_, subject, arms):
+                subject.free(set_)
+                for pattern, expr in arms:
+                    set_ += expr.free() - pattern.bind() - scope
+
+            case BreakStatement(_, _, expr):
+                if expr is not None:
+                    expr.free(set_)
+
+            case ContinueStatement(_, _):
+                pass
+
+            case ReturnStatement(_, inner):
+                inner.free(set_)
+
+            case _:
+                raise NotImplementedError(f"`{type(statement)}.free`")
+    return set_
 
 
 @dataclass
 class Statement(SyntaxNode):
-    def scope_free(self, set_=None, start_index=0) -> set[str]:
-        scope = set()
+    pass
 
-        for child in self.children[start_index:]:
-            match child.ty:
-                case StatementTy.Let:
-                    pat, expr = child.children
-                    pat.binds(scope)
-                    set_ += expr.free() - scope
 
-                case StatementTy.Assign:
-                    _pat, expr = child.children
-                    expr.free(set_)
+@dataclass
+class ExprStatement(Statement):
+    """
 
-                case StatementTy.Loop:
-                    child.scope_free(set_)
+    Example:
+        e
+    """
 
-                case StatementTy.Match:
-                    child.children[0].free(set_)
-                    child.scope_free(set_, start_index=1)
+    inner: Expr
 
-                case StatementTy.Break | StatementTy.Continue | StatementTy.Return:
-                    for child in self.children:
-                        child.free(set_)
 
-                case _:
-                    raise NotImplementedError
+@dataclass
+class LetStatement(Statement):
+    """
 
-        return set_
+    Example:
+        let p = e
+    """
+
+    pattern: "Pattern"
+    inner: Expr
+
+
+@dataclass
+class AssignStatement(Statement):
+    """
+
+    Example:
+        p = e
+    """
+
+    pattern: "Pattern"
+    inner: Expr
+
+
+@dataclass
+class LoopStatement(Statement):
+    """
+
+    Example:
+        loop { ... }
+    """
+
+    statements: list[Statement]
+
+
+@dataclass
+class MatchStatement(Statement):
+    """
+
+    Example:
+        match subject { arm... }
+    """
+
+    subject: Expr
+    arms: list[tuple["Pattern", Expr]]
+
+
+@dataclass
+class BreakStatement(Statement):
+    """
+
+    Example:
+        break ['label] [e]
+    """
+
+    label: Optional[Span]
+    inner: Optional[Expr]
+
+
+@dataclass
+class ContinueStatement(Statement):
+    """
+
+    Example:
+        continue ['label]
+    """
+
+    label: Optional[Span]
+
+
+@dataclass
+class ReturnStatement(Statement):
+    """
+
+    Example:
+        return [x]
+    """
+
+    inner: Optional[Expr]
 
 
 @dataclass
 class Pattern(SyntaxNode):
-    def binds(self, set_=None) -> set[str]:
-        if not isinstance(self.ty, PatternTy):
-            raise TypeError
-
+    def bind(self, set_=None) -> set[str]:
         if set_ is None:
             set_ = set()
+        match self:
+            case IdPattern(_, name, inner):
+                set_.add(name.str())
+                inner.bind(set_)
 
-        match self.ty:
-            case PatternTy.Id:
-                set_.add(self.span.str())
-                raise NotImplementedError
+            case ArrayPattern(_, items, _, _, _):
+                for item in items:
+                    item.bind(set_)
+
+            case GatherPattern(_, _, inner):
+                inner.bind(set_)
+
+            case (
+                IgnorePattern(_)
+                | TagPattern(_)
+                | IntPattern(_)
+                | FloatPattern(_)
+                | StringPattern(_)
+            ):
+                pass
 
             case _:
-                raise NotImplementedError
-
+                raise NotImplementedError(f"`{type(self)}.bind`")
         return set_
+
+
+@dataclass
+class IgnorePattern(Pattern):
+    """A pattern that matches anything
+
+    Example:
+        _name
+    """
+
+    pass
+
+
+@dataclass
+class IdPattern(Pattern):
+    """A pattern that matches anything, or whatever the inner pattern matches, and then binds the matched value to a new name
+
+    Example:
+        a[@p]
+    """
+
+    name: Span
+    inner: Optional[Pattern] = None
+
+
+@dataclass
+class TagPattern(Pattern):
+    """A pattern that matches a tag
+
+    Example:
+        :a
+    """
+
+    pass
+
+
+@dataclass
+class IntPattern(Pattern):
+    """A pattern that matches an integer
+
+    Example:
+        123
+    """
+
+    pass
+
+
+@dataclass
+class FloatPattern(Pattern):
+    """A pattern that matches a float value
+
+    Example:
+        123.456
+    """
+
+    pass
+
+
+@dataclass
+class StringPattern(Pattern):
+    """A pattern that matches a string
+
+    Example:
+        "Hello"
+    """
+
+    pass
+
+
+@dataclass
+class ArrayPattern(Pattern):
+    """A pattern that matches an array
+
+    Example:
+        [p0, ...p1, p2]
+    """
+
+    items: list[Pattern]
+    lsq: Span
+    commas: list[Span]
+    rsq: Span
+
+
+@dataclass
+class GatherPattern(Pattern):
+    """A pseudo-pattern that gathers remaining elements in an array or struct
+
+    May only appear within an `ArrayPattern`
+
+    Example:
+        ...p
+    """
+
+    ellipsis: Span
+    inner: Pattern
